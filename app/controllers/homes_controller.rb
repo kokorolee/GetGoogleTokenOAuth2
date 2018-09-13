83224cb57032dc564fe22bdc0da86f7a0e60fe57
Lê Hoàng Tâm <tamlh@atmarkcafe.org>
2018-09-13 13:59:30 +0700

    .

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,5 +3,9 @@ class HomesController < ApplicationController
   def index
     @token_adwords = Oauth2Token.where(access_token: session[:token_adwords]["access_token"]).first if session[:token_adwords]
     @token_analytics = Oauth2Token.where(access_token: session[:token_analytics]["access_token"]).first if session[:token_analytics].present?
+
+
+    @allToken = Oauth2Token.all.order(created_at: :DESC)
+
   end
 end
